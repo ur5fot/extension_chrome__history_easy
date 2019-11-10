dc371d0a47375211cbeecf632c9913167797791b
ur5fot <ur5fot@gmail.com>
2019-11-10 20:50:53 +0200

    feat ( popup ) add history list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as _ from 'lodash'
+import * as URLParse from 'url-parse'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'extchrombookmarks';
+  history;
+  structuredByDomain: any;
+  test = 1;
+
+  structuredStories(history) {
+    let structuredByDomain = [];
+    history.forEach((historyItem,) => {
+      let url = URLParse(historyItem.url).hostname;
+      let historyIndexOf = _.findIndex(structuredByDomain, {url});
+
+      if (historyIndexOf < 0) {
+        structuredByDomain.push({url, items: [historyItem]});
+      } else {
+        structuredByDomain[historyIndexOf].items.push(historyItem);
+      }
+    });
+
+    return structuredByDomain
+  }
+
+  ngOnInit() {
+
+      chrome.history.search({text: '', maxResults: 1000}, (history) => {
+        this.history = history;
+        this.structuredByDomain = this.structuredStories(history);
+
+      });
+
+    setTimeout(()=> document.documentElement.click(), 5000);
+  }
 }
---
+++ src/background.ts
@@ -0,0 +1,13 @@
+
+
+
+//
+// chrome.runtime.onInstalled.addListener(function() {
+//
+//   chrome.history.search({text: '', maxResults: 20}, (history) => {
+//       chrome.storage.sync.set({history}, function() {
+//       console.log('get history', history);
+//     });
+//   });
+//
+// });
